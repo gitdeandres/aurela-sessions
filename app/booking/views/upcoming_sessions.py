959e32754b07247943a902ebd32615e4bcf3ce18
Andrés <andresravelo.9@gmail.com>
2026-06-29 17:36:16 +0200

    fix: implement pagination for upcoming sessions response

--- app/booking/views/upcoming_sessions.py
+++ app/booking/views/upcoming_sessions.py
@@ -1,3 +1,4 @@
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,7 +8,9 @@ from app.booking.services import get_upcoming_sessions
 
 
 class UpcomingSessionsView(APIView):
-    def get(self, request: Request, pk: int) -> Response:
+    def get(self, request: Request, pk: id):
         sessions = get_upcoming_sessions(therapist_id=pk)
-        serializer = SessionSerializer(sessions, many=True)
-        return Response(serializer.data)
+        paginator = PageNumberPagination()
+        paginator.page_size = 20
+        page = paginator.paginate_queryset(sessions, request)
+        return paginator.get_paginated_response(SessionSerializer(page, many=True).data)
