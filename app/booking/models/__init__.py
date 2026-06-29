a3f3d1bcb295899a36f2814b1254b0e730ec129b
Andrés <andresravelo.9@gmail.com>
2026-06-29 12:05:24 +0200

    feat: add TERMINAL_STATUSES to the booking module

--- app/booking/models/__init__.py
+++ app/booking/models/__init__.py
@@ -1,5 +1,12 @@
 from .patient import Patient
-from .session import CancelledBy, Session, SessionStatus
+from .session import TERMINAL_STATUSES, CancelledBy, Session, SessionStatus
 from .therapist import Therapist
 
-__all__ = ['Therapist', 'Patient', 'Session', 'SessionStatus', 'CancelledBy']
+__all__ = [
+    'Therapist',
+    'Patient',
+    'Session',
+    'SessionStatus',
+    'CancelledBy',
+    'TERMINAL_STATUSES'
+]
